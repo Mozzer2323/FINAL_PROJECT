5eef3c76d2434d41d38a2c2313f7c8338ba12213
Lowden Hall <lowdenhall@gmail.com>
2019-11-14 10:26:12 -0600

    Navbar nearing complete, reorganized navbar, added icons, and made navbar fixed.

--- src/App.js
+++ src/App.js
@@ -1,55 +1,75 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Animated } from "react-animated-css";
+import 'font-awesome/css/font-awesome.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
-      <div class="navbar">
-        <div class="logo">
-      <img src={require('./images/bespokeLogow.png')} />
-      </div>
-      <div class="navmenu">
-        <ul>
-          <li><a class='no-link' href="#">SHOP</a>
-            <ul class="megamenu">
-              <li>
-                <h1>Title</h1>
-                <a href="#">Submenu 1</a>
-                <a href="#">Submenu 2</a>
-                <a href="#">Submenu 3</a>
-                <a href="#">Submenu 4</a>
-                <a href="#">Submenu 5</a>
-                <a href="#">Submenu 6</a>
-                <a href="#">Submenu 7</a>
-              </li>
-            </ul>
-           </li>   
+    <Animated animationIn="fadeIn slower" isVisible={true}>
+      <div className="App">
+        <div class="fixed-top">
+          <div class="navbar">
+            <div class="navmenu">
+              <ul>
+                <li><a class='no-link' href="#">SHOP</a>
+                  <ul class="megamenu">
+                    <li>
+                      <h1>Apparrel</h1>
+                      <a href="#">T-Shirts</a>
+                      <a href="#">Sweaters</a>
+                      <a href="#">Coats</a>
+                      <a href="#">Jackets</a>
+                      <a href="#">Pants</a>
+                      <a href="#">Shorts</a>
+                      <a href="#">Jeans</a>
+                    </li>
+                  </ul>
+                </li>
                 <li><a class='no-link' href="#">ABOUT</a></li>
                 <li><a class='no-link' href="#">HELP</a></li>
+                <div class="logo">
+                  <img src={require('./images/bespokeLogow.png')} />
+                </div>
                 <li><a class='no-link' href="#">LOGIN</a></li>
-                <li><a class='no-link' href="#">CART</a></li>
-                </ul>
-           </div>
-      
-      <br></br>
-      <br></br>
-
-      <div class="indexwrap">
-      <div class="discoverwrap">
-        <div class="headlinebox">
-          <div class="headlinetxt">
-          <h1>Shop Our Latest Winter Arrivals</h1>
-          <div class="headlinesub">
-            <p>Dress (adjective) this season with new arrivals from Maison Margiela, Rick Owens, Burberry, and more.</p>
+                <li><a class='no-link' href="#"><i className="fa fa-shopping-bag fa-lg"></i></a></li>
+                <li><a class='no-link' href="#"><i className="fa fa-search fa-lg"></i></a></li>
+              </ul>
+            </div>
           </div>
+        </div>
+        <br></br>
+        <div class="indexwrap">
+          <div class="discoverwrap">
+            <div class="headlinebox">
+              <div class="headlinetxt">
+                <h1>Shop Our Latest Winter Arrivals</h1>
+                <div class="headlinesub">
+                  <p>Dress (adjective) this season with new arrivals from Maison Margiela, Rick Owens, Burberry, and more.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <br></br>
+
+        <div class="indexwrap">
+          <div class="discoverwrap">
+            <div class="headlinebox">
+              <div class="headlinetxt">
+                <h1>Shop Our Latest Winter Arrivals</h1>
+                <div class="headlinesub">
+                  <p>Dress (adjective) this season with new arrivals from Maison Margiela, Rick Owens, Burberry, and more.</p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
-</div>
+    </Animated>
+
+
   );
 }
 
