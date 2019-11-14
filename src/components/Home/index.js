9de2e4b73344b4a0b758754d9586fdf6e7feb13d
DanielGerhart <djgerhart@outlook.com>
2019-11-14 08:49:15 -0600

    added component folder

---
+++ src/components/Home/index.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css';
+
+function Home() {
+  return (
+    <div className="App">
+
+      <div class="navbar">
+        <div class="logo">
+      <img src={require('./images/bespokeLogow.png')} />
+      </div>
+      <div class="navmenu">
+        <ul>
+          <li><a class='no-link' href="#">SHOP</a>
+            <ul class="megamenu">
+              <li>
+                <h1>Title</h1>
+                <a href="#">Submenu 1</a>
+                <a href="#">Submenu 2</a>
+                <a href="#">Submenu 3</a>
+                <a href="#">Submenu 4</a>
+                <a href="#">Submenu 5</a>
+                <a href="#">Submenu 6</a>
+                <a href="#">Submenu 7</a>
+              </li>
+            </ul>
+           </li>   
+                <li><a class='no-link' href="#">ABOUT</a></li>
+                <li><a class='no-link' href="#">HELP</a></li>
+                <li><a class='no-link' href="#">LOGIN</a></li>
+                <li><a class='no-link' href="#">CART</a></li>
+                </ul>
+           </div>
+      
+      <br></br>
+      <br></br>
+
+      <div class="indexwrap">
+      <div class="discoverwrap">
+        <div class="headlinebox">
+          <div class="headlinetxt">
+          <h1>Shop Our Latest Winter Arrivals</h1>
+          <div class="headlinesub">
+            <p>Dress (adjective) this season with new arrivals from Maison Margiela, Rick Owens, Burberry, and more.</p>
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+  );
+}
+
+export default Home;
